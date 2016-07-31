eb4bad9497165153f3e3db6a5979f66a11c84e34
kalechao87 <kalechao@outlook.com>
2016-07-31 22:02:51 +0800

    logomove
    logomove

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@
   Author: Kale Chao | FakeCityMan
   Blog: http://kalechao87.github.io/
 **/
+var jumpTime = 0.8;
 // 预加载
 var sourceArr = [
     'images/universal-bg.jpg',
@@ -46,7 +47,8 @@ new mo.Loader(sourceArr,{
             onComplete: function () {
                 // TweenMax.set('#music-control', {display: 'block', autoAlpha: 1});
                 // bgAud.play();
-                // showCover();
+                // showCity();
+                logoShake1.play(0);
 
             }
         });
@@ -63,6 +65,96 @@ function setImages() {
     $('#arrow-up').css('background-image', 'url(images/arrow-up.png)');
     $('#arrow-left').css('background-image', 'url(images/arrow-left.png)');
 }
+
+// 初始抖动
+var logoShake1 = new TimelineMax({
+    paused: true,
+    repeat: 11,
+    yoyo: true,
+    onComplete: showCity
+});
+logoShake1.to('#logo', 0.1, {x: -20, ease: Power1.easeInOut});
+
+function showCity() {
+    var cityShow = new TimelineMax({
+        onComplete: moveLogo
+    });
+    cityShow.set('#city', {autoAlpha: 1})
+    .add('tencent')
+    .fromTo('#city', 0.8, {autoAlpha: 0, y: 640}, {autoAlpha: 1, y: 0}, 'tencent')
+    .to('#logo', 0.6, {
+        x: '-=30',
+        y: '-=160',
+        ease: Back.easeIn.config(1.6),
+        onComplete: function () {
+            logoRotation.play(0);
+        }
+    }, 'tencent')
+}
+
+// logo 旋转
+var logoRotation = new TimelineMax({
+    paused: true,
+    repeat: -1
+});
+logoRotation.to('#logo', 1.2, {rotation: 360, ease: Power0.easeNone});
+
+function moveLogo() {
+    var logoMove = new TimelineMax();
+    logoMove.to('#logo', jumpTime, {
+        bezier:
+        [
+            {x:'+=40', y: '-=80'},
+            {x: '+=240', y: '+=200'}
+        ],
+
+        ease: Back.easeIn.config(1.2)
+    })
+    .to(['#city', '#logo'], 0.6, {x: '-=290', y: '-=40'})
+    .to('#logo', 0.6, {x: '+=270', y: '+=100'})
+    .to(['#city', '#logo'], 0.6, {x: '-=290', y: '-=300'})
+    .to('#logo', jumpTime, {
+        bezier:
+        [
+            {x:'+=80', y: '-=180'},
+            {x: '+=330', y: '-=290'}
+        ],
+
+        ease: Back.easeIn.config(1.2)
+    })
+    .to(['#city', '#logo'], 0.6, {x: '-=460'})
+    .to('#logo', jumpTime, {
+        bezier:
+        [
+            {x:'+=200', y: '-=40'},
+            {x: '+=510', y: '+=350'}
+        ],
+
+        ease: Back.easeIn.config(1.2)
+    })
+    .to(['#city', '#logo'], 0.6, {x: '-=400', y: '-=80'})
+    .to('#logo', jumpTime, {
+        bezier:
+        [
+            {x:'+=100', y: '-=100'},
+            {x: '+=280', y: '+=500'}
+        ],
+
+        ease: Back.easeIn.config(1.2)
+    })
+    .to('#logo', jumpTime, {
+        bezier:
+        [
+            {x:'+=80', y: '+=16'},
+            {x: '+=220', y: '+=220'}
+        ],
+
+        ease: Back.easeIn.config(1.2)
+    })
+
+}
+
+
 
 (function($) {
     $(document).ready(function() {
